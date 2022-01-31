841aeb83ae1b721fb9e312046c5ffda2f143094e
nijo dev <nxvdownload@gmail.com>
2022-01-31 22:13:21 +0100

    Add solution skeleton + .gitignore file

---
+++ src/FSharpToolBox/Program.fs
@@ -0,0 +1,7 @@
+﻿open FSharpToolBoxLib
+
+// For more information see https://aka.ms/fsharp-console-apps
+printfn "FSharpToolBox - Begin explore"
+
+// Test using FSharpToolBoxLib
+helloLib "Nijo"
---
+++ src/Lib/FSharpToolBoxLib.fs
@@ -0,0 +1,10 @@
+﻿// namespace FSharpToolBoxLib
+
+// module Say =
+//     let hello name =
+//         printfn "Hello %s" name
+
+module FSharpToolBoxLib
+
+let helloLib name =
+    printfn "Hello from FSharpToolBoxLib %s" name
